7f02cf2bb85ce3c099ce8adc803650ee1f8d3148
Niyihoraho <heroicrwand@gmail.com>
2024-03-26 00:01:44 +0200

    responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,96 +1,153 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import "./footer.css";
-import { FiChevronRight} from "react-icons/fi";
+import { FiChevronRight } from "react-icons/fi";
+import { FiSend } from "react-icons/fi";
+import { AiFillFacebook, AiFillInstagram, AiOutlineTwitter, AiOutlineYoutube } from "react-icons/ai";
 import { MdOutlineTravelExplore } from "react-icons/md";
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import image from "../../set/f1.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Footer = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1200 });
+  }, []);
 
-  useEffect(() =>{
-    Aos.init({duration: 1200})
-  },[]);
-  
   return (
-    <div>
+    
       <section className="footer">
-        <div className="container">
-          <div className="contact">
-            <div className="left" data-aos="fade-up">
-              <h2 >Keep in Touch</h2>
-              <h1 >Travel with Us</h1>
+
+        <div className="setContent container">
+          <div className="contactDiv flex">
+            <div className="text" data-aos="fade-up">
+              <small>KEEP IN TOUCH</small>
+              <h2>Travel with us</h2>
             </div>
-            <div className="input" data-aos="fade-up">
-              <input type="text" placeholder="enter email address" />
-            </div>
-            <div className="btn" data-aos="fade-up">
-              <button type="Submit">Send</button>
+            <div className="inputDiv flex">
+              <input type="text" placeholder="Enter Email Address" data-aos="fade-up" />
+              <button className="btn flex" type="submit" data-aos="fade-up">
+                SEND
+                <FiSend className="icon" />
+              </button>
             </div>
           </div>
-          
-          <div className="card" >
-            <div className="info">
-            <div className="left-side">
-              <div className="logo">
-                <MdOutlineTravelExplore className="icon" data-aos="fade-up"/>
-                Travel.
+          <div className="footercard flex">
+            <div className="footerIntro flex">
+              <div className="logoDiv">
+                <a href="#" className="logo flex">
+                  <MdOutlineTravelExplore className="icon" /> travel.
+                </a>
               </div>
-              <div className="text" data-aos="fade-up">
-                <p>
-                  flexboxThis course is created to help you to learning
-                  basics of flexbox it useful tools used in css This course is
-                  flexboxThis course is created to help you to learning
-                  basics of flexbox it useful tools used in css This course is
-                  created to help you This course is created to help you to
-                  learning basics of flexbox
-                </p>
-              </div>
-              <div className="social-media"></div>
-            </div>
-            <div className="right-side" data-aos="fade-up">
-          
-              <ul>
-                <li>
-                  <h2>AGENCY</h2>
-                </li>
-                <li><FiChevronRight className="icon" />Service</li>
-                <li><FiChevronRight className="icon"/>Insurance</li>
-                <li><FiChevronRight className="icon"/>Agency</li>
-                <li><FiChevronRight className="icon"/>Tourism</li>
-                <li><FiChevronRight className="icon"/>Payment</li>
-              </ul>
 
-              <ul>
-                <li>
-                  <h2>Partners</h2>
-                </li>
-                <li><FiChevronRight className="icon"/>Booking</li>
-                <li><FiChevronRight className="icon"/>RentalCar</li>
-                <li><FiChevronRight className="icon"/>HostelWorld</li>
-                <li><FiChevronRight className="icon"/>Trivago</li>
-                <li><FiChevronRight className="icon"/>TripAdvisor</li>
-              </ul>
-              <ul>
-                <li>
-                  <h2>Services</h2>
-                </li>
-                <li><FiChevronRight className="icon"/>Booking</li>
-                <li><FiChevronRight className="icon"/>RentalCar</li>
-                <li><FiChevronRight className="icon"/>HostelWorld</li>
-                <li><FiChevronRight className="icon"/>Trivago</li>
-                <li><FiChevronRight className="icon"/>TripAdvisor</li>
-              </ul>
+              <div className="footerParagraph" data-aos="fade-up">
+                flexboxThis course is created to help you to learning basics of
+                flexbox it useful tools used in css This course is flexboxThis
+                course is created to help you to learning basics of flexbox it
+                useful tools used in css This course is created to help you This
+                course is created to help you to learning basics of flexbox
+              </div>
+              <div className="footerSocials flex" data-aos="fade-up">
+               <AiOutlineTwitter className="icon"/>
+               <AiOutlineYoutube className="icon"/>
+               <AiFillInstagram className="icon"/>
+               <AiFillFacebook className="icon"/>
+              </div>
 
             </div>
+
+           <div className="footerLinks grid">
+{/* ------------------------------------------------------- */}
+            <div className="linkGroup" data-aos="fade-up">
+              <span className="title">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
             </div>
-            <div className="copy">
-            <div className="copyright">
-              &copy;Copyrights Reserved-Heroic 2024
-              </div>
-              </div>
+            <div className="linkGroup">
+              <span className="title">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+            </div>
+            <div className="linkGroup">
+              <span className="title">
+                OUR AGENCY
+              </span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon"/>
+                Payment
+              </li>
+            </div>
+            {/* ---------------------------------------------------------- */}
+            </div> 
+
+
+
+
+  
+          <div className="footerDiv flex">
+            <small>&copy;CopyRight Reserved -artDEVELOPER</small>
+          </div>
           </div>
         </div>
       </section>
-    </div>
+  
   );
 };
 
